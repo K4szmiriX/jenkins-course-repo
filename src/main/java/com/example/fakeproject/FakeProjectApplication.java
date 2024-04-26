b70de452a0c8b348b978c335493d5093acfed4dd
Piotr Michałek <piotr.michalek@altar.com.pl>
2024-04-26 15:43:24 +0200

    first commit

---
+++ src/main/java/com/example/fakeproject/FakeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.fakeproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FakeProjectApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FakeProjectApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/fakeproject/FakeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fakeproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FakeProjectApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
